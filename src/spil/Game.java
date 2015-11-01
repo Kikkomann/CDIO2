1b0b08dbce380f0bc449d5577a15414a34f6a0a6
Rune Hou Thode <runehouthode@gmail.com>
2015-11-01 18:03:44 +0100

    Beskrivelser til felterne

--- src/spil/Game.java
+++ src/spil/Game.java
@@ -10,29 +10,58 @@ public class Game {
 	
 	public Game(){
 		field = new Field[11];
-		field[0] = new Field("Tower", "", 250, false);
-		field[1] = new Field("Crater", "", -100, false);
-		field[2] = new Field("Palace Gates", "", 100, false);
-		field[3] = new Field("Cold Deserts", "", -20, false);
-		field[4] = new Field("Walled City", "", 180, false);
-		field[5] = new Field("Monastery", "", 0, false);
-		field[6] = new Field("Black Cave", "", -70, false);
-		field[7] = new Field("Huts in the Mountains", "", 60, false);
-		field[8] = new Field("The Werewall", "", -80, true);
-		field[9] = new Field("The Pit", "", -50, false);
-		field[10] = new Field("Goldmine", "", 650, false);
+		field[0] = new Field("Tower", "You find a tower and at the top of the tower's stairs someone forgot his wallet. Wow is not bad!", 250, false);
+		field[1] = new Field("Crater", "As you walk in the forest you are not paying attention to the path and fall into a crater. You can't get up yourself and after two hours of crying for help, someone finds you and helps you up. He charge you 100 for the help", -100, false);
+		field[2] = new Field("Palace Gates", "You reach the Palace Gates and say hello to your cousin who owes you 100.", 100, false);
+		field[3] = new Field("Cold Deserts", "You want to see the Cold Deserts, but forgot a warm hat. Luckily a new one only costs 20.", -20, false);
+		field[4] = new Field("Walled City", "The Walled City is a must-see in this kingdom. You decide to visit the city. A shame that the price reflects the popularity. You pay 180 to enter.", 180, false);
+		field[5] = new Field("Monastery", "On your journey you come across a monastery. Monks are boring, so you walk past and hope for something more interesting next time.", 0, false);
+		field[6] = new Field("Black Cave", "You come to the end of the forest path and find a black cave. You are getting hungry and buy a sandwich at the Sandwich Man before you explore the cave. It is a delicious sandwich and it only costed you 70.", -70, false);
+		field[7] = new Field("Huts in the Mountains", "You decided to go to the mountains and discover some huts. People like you so much that they want to give you a present. It is an ugly statue that you sell for 60.", 60, false);
+		field[8] = new Field("The Werewall", "You don't realize that you have come to the dangerous area in the kingdom - the Werewall. The werewolfes spot you and tries to catch you. You try to pay you out of the situation, but apparently 80 is not enough to help you. You run as fast as you can, and moves on quickly. You better roll again.", -80, true);
+		field[9] = new Field("The Pit", "It is getting late and the only place that is open now is The Pit. Tonights theme adventure-party. Entrance is 50 and you go in and party all night.", -50, false);
+		field[10] = new Field("Goldmine", "You spot a goldmine full of gold. You take as much as you can and sell it all for 650.", 650, false);
 		
 		dicecup = new DiceCup();
 		player = new Player[2];
-		player[0] = new Player("p1");
-		player[1] = new Player("p2");
+		player[0] = new Player(GUI.getUserString("Choose a name for player 1:"));
+		player[1] = new Player(GUI.getUserString("Choose a name for player 2:"));
+		GUI.addPlayer(player[0].getName(), player[0].getBalance());
+		GUI.addPlayer(player[1].getName(), player[1].getBalance());
 	}
 	
 	private void run(){
+		int turn = 0;
+		Player cplayer = player[turn]; //current player
 		
+		while(!checkWinner())
+		{
+			GUI.getUserButtonPressed(cplayer.getName() + ", your turn.", "Roll");
+			dicecup.roll();
+			GUI.setDice((dicecup.getDiceValues())[0], (dicecup.getDiceValues())[1]);
+			field[(dicecup.getDiceSum()) - 2].land(cplayer);
+			GUI.showMessage(field[(dicecup.getDiceSum()) - 2].getDescription());
+			
 		
-		
+		}
 	}
 	
+	
+	
+	private boolean checkWinner(){
+		if(player[0].getBalance() >= 3000 || player[0].getBalance() >= 3000)
+			return true;
+		
+		else
+			return false;
+	}
+	
+	
+	public static void main(String[] args) {
+		Game game = new Game();
+		
+		game.run();
+
+	}
 	
 }
